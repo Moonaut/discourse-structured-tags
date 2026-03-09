bc8e91f0e797e615965df6ec23c6f8657389255f
paulovbettio <paulovinicius.bettio@gmail.com>
2026-03-08 21:22:37 -0400

    Fix milestone version display name helper (_ is not used anymore, swapped for -)

--- javascripts/discourse/helpers/tag-display-name.js
+++ javascripts/discourse/helpers/tag-display-name.js
@@ -2,7 +2,7 @@ import { helper } from "@ember/component/helper";
 
 export default helper(function tagDisplayName([name]) {
   if (name?.startsWith("milestone:")) {
-    return name.replaceAll("_", ".");
+    return name.replaceAll("-", ".");
   }
   return name;
 });
